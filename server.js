87d3fbe4717d36ec641fb83fe03fff575bcbb7d2
Spencer Powell <spencerp@Spencers-MacBook-Pro-2.local>
2017-01-02 15:42:20 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+var currentYear = new Date().getFullYear();
+
+// line added to allow for partials in handlebars
+hbs.registerPartials(__dirname + '/views/partials');
+// changes express to use Handlebars as the view engine
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// // Maintenance Mode
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Under Construction',
+//     welcomeMessage: 'We will be back soon!'
+//   });
+// });
+
+// allows html pages to run without having to create a path to each one
+app.use(express.static(__dirname + '/public'));
+
+// sets up a helper function for hbs
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+  // res.send('<h1>hello express</h1>');
+  // res.send({
+  //   name: 'Spencer',
+  //   likes: [
+  //     'Biking',
+  //     'Cities'
+  //   ]
+  // })
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'This year is THE year!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    // currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to process request'
+  });
+});
+
+app.listen(3000, () =>{
+  console.log('Server is up on port 3000');
+});
