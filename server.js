5d715e7ee5961ee77de71936cdd618be65767b14
Spencer Powell <spencerp@Spencers-MacBook-Pro-2.local>
2017-01-02 20:28:59 -0700

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+// enviornment variable (PORT) from OS for Heroku
+const port = process.env.PORT || 3000;
 
 var app = express();
 var currentYear = new Date().getFullYear();
@@ -71,6 +73,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () =>{
-  console.log('Server is up on port 3000');
+app.listen(port, () =>{
+  console.log(`Server is up on port ${port}`);
 });
