1929716a9f30ea721bf935f0388ea34054931782
Spencer Powell <spencerp@Spencers-MacBook-Pro-2.local>
2017-01-02 21:26:50 -0700

    added the projects page

--- server.js
+++ server.js
@@ -67,6 +67,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    // currentYear: new Date().getFullYear()
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to process request'
